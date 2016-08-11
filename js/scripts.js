bc42cd0fd3b67bad34751cdcfa20e49d33989490
Elysia Nason and Sandro Alvarez <elysia@>
2016-08-11 14:16:01 -0700

    add radio buttons to rest of form and else if branches to determine celebrity date

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,24 @@
 $(function() {
   $("form").submit(function(event) {
-    var age = parseInt($("#age").val();
-    var gender = parseInt($(".gender").val());
-    var place = parseInt($(".place").val())
-
     event.preventDefault();
+    var age = parseInt($("#age").val());
+    var gender = parseInt($(".gender").val());
+    var place = parseInt($(".place").val());
+    var meal = parseInt($(".meal").val());
+    var creature = parseInt($(".creature").val());
+    var counter = gender + place + meal + creature;
+    if (age < 18 && counter === 0)  {
+      $("#kanye").show();
+    } else if (age >= 18 && 0<counter<=4) {
+      $("#bieber").show();
+    } else if (age >= 18 && 4<counter<=8) {
+      $("#nicki").show();
+    } else if (age >= 18 && 8<counter<11) {
+      $("#rihanna").show();
+    } else if (counter === 11) {
+      $("#ruby").show();
+    } else {
+      $("#kanye").show();
+    }
   });
 });
